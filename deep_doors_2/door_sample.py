f0dc26370fdd2f7bf32c10416750f4368d53f547
micheleantonazzi <m7242872428>
2021-07-26 17:56:47 +0200

    door_sample.py: fixed visualize method and depth image load method

--- deep_doors_2/door_sample.py
+++ deep_doors_2/door_sample.py
@@ -1,13 +1,14 @@
-import os
-from typing import List, NoReturn, Tuple
+from typing import NoReturn
 
 import cv2
 import numpy as np
 from generic_dataset.data_pipeline import DataPipeline
 from generic_dataset.generic_sample import synchronize_on_fields
 from generic_dataset.sample_generator import SampleGenerator
-from generic_dataset.utilities.save_load_methods import save_compressed_dictionary, load_compressed_dictionary, save_cv2_image_bgr, load_cv2_image_bgr, save_compressed_numpy_array, load_compressed_numpy_array
+from generic_dataset.utilities.save_load_methods import save_cv2_image_bgr, load_cv2_image_bgr, \
+    save_compressed_numpy_array, load_compressed_numpy_array, load_cv2_image_grayscale
 
+COLORS = [(0, 0, 255), (255, 0, 0), (0, 255, 0)]
 
 pipeline_fix_gbr_image = DataPipeline().add_operation(lambda d, e: (d[..., [2, 1, 0]], e))
 
@@ -17,18 +18,17 @@ def visualize(self) -> NoReturn:
     This method visualizes the sample, showing all its fields.
     :return:
     """
-    print(f'Label: {self.get_label()}')
-    print(f'Robot pose: {self.get_robot_pose()}')
     bgr_image = self.get_bgr_image()
     depth_image = self.get_depth_image()
-    semantic_image = self.get_semantic_image()
-    pretty_semantic_image = self.get_pretty_semantic_image()
+    img_bounding_boxes = bgr_image.copy()
+
+    for label, *box in self.get_bounding_boxes():
+        cv2.rectangle(img_bounding_boxes, box, color=COLORS[label], thickness=1)
 
     row_1 = np.concatenate((bgr_image, cv2.cvtColor(depth_image, cv2.COLOR_GRAY2BGR)), axis=1)
-    row_2 = np.concatenate((semantic_image, pretty_semantic_image), axis=1)
-    image = np.concatenate((row_1, row_2), axis=0)
+    row_1 = np.concatenate((row_1, img_bounding_boxes), axis=1)
 
-    cv2.imshow('Sample', image)
+    cv2.imshow('Sample', row_1)
     cv2.waitKey()
 
 
@@ -39,7 +39,7 @@ DOOR_LABELS = {0: 'Closed door', 1: 'Semi opened door', 2: 'Opened doorw'}
 
 DoorSample = SampleGenerator(name='DoorSample', label_set={0, 1}) \
     .add_dataset_field(field_name='bgr_image', field_type=np.ndarray, save_function=save_cv2_image_bgr, load_function=load_cv2_image_bgr) \
-    .add_dataset_field(field_name='depth_image', field_type=np.ndarray, save_function=save_cv2_image_bgr, load_function=load_cv2_image_bgr) \
+    .add_dataset_field(field_name='depth_image', field_type=np.ndarray, save_function=save_cv2_image_bgr, load_function=load_cv2_image_grayscale) \
     .add_dataset_field(field_name='bounding_boxes', field_type=np.ndarray, default_value=np.array([]), load_function=load_compressed_numpy_array, save_function=save_compressed_numpy_array) \
     .add_custom_method(method_name='visualize', function=visualize) \
     .generate_sample_class()
